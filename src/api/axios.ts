a03c471b53d00247c5e347b1355a3ad32aa88b11
Elpacho <akunov313131@gmail.com>
2025-10-10 13:30:09 +0600

    прототип

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://172.20.10.2:3001/api",
+    baseURL: "https://shailo-server.vercel.app/api",
     withCredentials: true, // чтобы куки передавались
 });
 
